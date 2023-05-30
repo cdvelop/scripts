0324b0b7374e58b7c8ae8fac125d198e655d75b6
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-05-30 11:37:40 -0400

    fix go-push test folder

--- go-push.sh
+++ go-push.sh
@@ -18,7 +18,9 @@ if [ -f "go.mod" ]; then
   # Buscar carpetas que contengan nombre test y ejecutar tests
   test_folders=$(find -type d -name "*test*")
   for folder in $test_folders; do
-   execute "go test $folder" "Hubo errores en las pruebas carpeta $folder en $go_mod_name" "folder test ok"
+     if [ -n "$(find $folder -type f -name "*_test.go")" ]; then
+      execute "go test $folder" "Hubo errores en las pruebas carpeta $folder en $go_mod_name" "$go_mod_name carpeta: $folder test ok"
+     fi   
   done
 
   # Ejecutar prueba de carrera de datos con go run
