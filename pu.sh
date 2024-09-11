762046dc2da104ce582019079857390d55c47f0c
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2024-09-11 00:40:30 -0300

    auto update package

--- pu.sh
+++ pu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source functions.sh
 
-# este script genera una etiqueta con un número correlativo cambiando solo el último
+# Este script genera una etiqueta con un número correlativo cambiando solo el último
 # dígito del tag ej v5.4.2 el siguiente será v5.4.3
 
 current_folder=$(basename "$(pwd)")
@@ -22,7 +22,7 @@ if [ -f "changes.txt" ] && [ -s "changes.txt" ]; then
 fi
 
 # Si commit_message está vacío, asigna un valor predeterminado
-if [ -z "$commit_message" ];then
+if [ -z "$commit_message" ]; then
     commit_message="auto update package"
 fi
 
@@ -36,53 +36,39 @@ else
     execute "git commit -m '$commit_message'" "Error al crear el nuevo commit $current_folder."
 fi
 
-# Verifica si el commit más reciente ha sido empujado
-ahead_count=$(git rev-list --count HEAD..@{u} 2>/dev/null)
+# Obtén la última etiqueta
+latest_tag=$(git describe --abbrev=0 --tags 2>/dev/null)
 
-if [ $? -ne 0 ]; then
-    echo "La rama no tiene upstream configurado."
-    ahead_count=1
+if [ -z "$latest_tag" ]; then
+    # Si no existe ninguna etiqueta, establece la etiqueta inicial en v0.0.1
+    new_tag="v0.0.1"
+else
+    # Extrae el número de la etiqueta
+    last_number=$(echo "$latest_tag" | grep -oE '[0-9]+$')
+
+    # Incrementa el número en uno
+    next_number=$((last_number + 1))
+
+    # Construye la nueva etiqueta
+    new_tag=$(echo "$latest_tag" | sed "s/$last_number$/$next_number/")
 fi
 
-if [ "$ahead_count" -gt 0 ]; then
-    echo "Hay commits sin empujar, se realizará el push."
+execute "git tag $new_tag" "Error al crear la nueva etiqueta $current_folder." "nueva etiqueta $new_tag"
 
-    # Obtén la última etiqueta
-    latest_tag=$(git describe --abbrev=0 --tags 2>/dev/null)
+# Verifica si la rama tiene upstream configurado
+branch=$(git symbolic-ref --short HEAD)
+upstream=$(git rev-parse --symbolic-full-name --abbrev-ref @{u} 2>/dev/null)
 
-    if [ -z "$latest_tag" ]; then
-        # Si no existe ninguna etiqueta, establece la etiqueta inicial en v0.0.1
-        new_tag="v0.0.1"
-    else
-        # Extrae el número de la etiqueta
-        last_number=$(echo "$latest_tag" | grep -oE '[0-9]+$')
-
-        # Incrementa el número en uno
-        next_number=$((last_number + 1))
-
-        # Construye la nueva etiqueta
-        new_tag=$(echo "$latest_tag" | sed "s/$last_number$/$next_number/")
-    fi
-
-    execute "git tag $new_tag" "Error al crear la nueva etiqueta $current_folder." "nueva etiqueta $new_tag"
-
-    # Verifica si la rama tiene upstream configurado
-    branch=$(git symbolic-ref --short HEAD)
-    upstream=$(git rev-parse --symbolic-full-name --abbrev-ref @{u} 2>/dev/null)
-
-    if [ -z "$upstream" ]; then
-        # Si no hay upstream, configura el upstream y haz push
-        execute "git push --set-upstream origin $branch && git push origin $new_tag" \
-        "Error al empujar los cambios y la nueva etiqueta a remoto $current_folder." \
-        "Commit y Push $current_folder..."
-    else
-        # Si ya tiene upstream, simplemente haz push
-        execute "git push && git push origin $new_tag" \
-        "Error al empujar los cambios y la nueva etiqueta a remoto $current_folder." \
-        "Commit y Push $current_folder..."
-    fi
+if [ -z "$upstream" ]; then
+    # Si no hay upstream, configura el upstream y haz push
+    execute "git push --set-upstream origin $branch && git push origin $new_tag" \
+    "Error al empujar los cambios y la nueva etiqueta a remoto $current_folder." \
+    "Commit y Push $current_folder..."
 else
-    echo "No hay commits que empujar, todo está sincronizado con el remoto."
+    # Si ya tiene upstream, simplemente haz push
+    execute "git push && git push origin $new_tag" \
+    "Error al empujar los cambios y la nueva etiqueta a remoto $current_folder." \
+    "Commit y Push $current_folder..."
 fi
 
 # Imprimir los mensajes acumulados
